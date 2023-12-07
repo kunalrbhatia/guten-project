4577d2f1815aba891251a27f8e8db528744cdeba
Kunal Bhatia <kunal.bhaia@hotmail.com>
2023-12-07 12:09:45 +0530

    added preloader for book cover

--- src/components/IndividualBook/IndividualBook.tsx
+++ src/components/IndividualBook/IndividualBook.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { get, startCase, upperCase } from 'lodash';
 import './IndividualBook.styles.css';
+
 interface Book {
   id: number;
   title: string;
@@ -8,19 +9,45 @@ interface Book {
   cover: string | undefined;
   onClick: () => void;
 }
-const IndividualBook = ({ id, title, authors, cover, onClick }: Book) => {
+
+const IndividualBook: React.FC<Book> = ({
+  id,
+  title,
+  authors,
+  cover,
+  onClick,
+}) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   let author = null;
   if (Array.isArray(authors) && authors.length > 0) {
     author = get(authors, '0', null) || null;
   }
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div key={id} onClick={onClick} className="cursor-pointer" title={title}>
       {cover && (
-        <img
-          src={cover}
-          alt={`${title} Cover`}
-          className="mb-2 w-[114px] h-[162px] object-cover rounded-[8px] custom-shadow"
-        />
+        <div
+          className={`w-[114px] h-[162px] mb-2 relative overflow-hidden rounded-[8px] custom-shadow`}
+        >
+          {!imageLoaded && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-500"></div>
+            </div>
+          )}
+          <img
+            src={cover}
+            alt={`${title} Cover`}
+            className={`w-full h-full object-cover ${
+              imageLoaded ? 'opacity-100' : 'opacity-0'
+            }`}
+            onLoad={handleImageLoad}
+          />
+        </div>
       )}
       <h3 className="text-extraSmall font-Montserrat-Regular mb-1 truncate">
         {upperCase(title)}
